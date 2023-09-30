51bd33857930baf6600c356ef2d5f93d998623fa
Sergey Dobrovolskiy <SFrontEnd@yandex.ru>
2023-10-01 01:18:50 +0300

    Добавляет env переменные

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT, DB_URL } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 const limiter = rateLimit({
